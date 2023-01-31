db26da7b144c7de3c22f47a864acbfcb9cb862a6
Matthew Norton <matthew.lee.norton@gmail.com>
2023-01-31 08:34:23 -0700

    commit

--- Mission04_norty144/wwwroot/js/GradeCalc.js
+++ Mission04_norty144/wwwroot/js/GradeCalc.js
@@ -1,26 +1,29 @@
 ﻿$("#submit").click(function () {
-
-
+    
+    //setting the weights of the sections for the class grades, parsed to floats
     var assignWeight = parseFloat(0.5);
-    var groupWeight = pareseFloat(0.1);
+    var groupWeight = parseFloat(0.1);
     var quizWeight = parseFloat(0.1);
     var midWeight = parseFloat(0.1);
     var finalWeight = parseFloat(0.1);
     var intexWeight = parseFloat(0.1);
-
     
+    //accepting the inputs from the form and setting them as new variables to use in this js section
     var assignP = parseFloat($("#assignmentsIN").val());
     var groupP = parseFloat($("#groupIN").val());
     var quizP = parseFloat($("#quizIN").val());
     var midP = parseFloat($("#midtermIN").val());
     var finalP = parseFloat($("#finalIN").val());
     var intexP = parseFloat($("#intexIN").val());
-
+    
+    //total weight calculated
     var totalWeight = assignWeight + groupWeight + quizWeight + midWeight + finalWeight + intexWeight;
 
+
+    //the final grade percentage is calculated here
     var finalGrade = (assignP * assignWeight + groupP * groupWeight + quizP * quizWeight + midP * midWeight + finalP * finalWeight + intexP * intexWeight) / totalWeight;
     
-
+    //setting of letterGrade variable and the if statements to decide the letter grade
     var letterGrade;
 
     if (finalGrade >= 94) {
@@ -49,7 +52,13 @@
         letterGrade = "E";
     }
 
-    $("#gradeP").html(letterGrade);
-    $("#letter").html(finalGrade);
+    //finalGrade is the final percentage***********
+    //letterGrade is the final letter grade************
+
+    //$("#gradeP").html(letterGrade);
+    //$("#letter").html(finalGrade);
+    output = finalGrade + "% " + letterGrade
+    $("#gradeP").html(output);
+    
 
 });
