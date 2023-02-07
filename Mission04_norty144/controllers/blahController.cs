eeee7e0b08e69b038e135e0679d7ad2b4f1fad5b
Matthew Norton <matthew.lee.norton@gmail.com>
2023-02-06 19:55:51 -0700

    assignment 5

---
+++ Mission04_norty144/controllers/blahController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission04_norty144.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_norty144.controllers
+{
+    public class blahController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult calculator()
+        {
+
+            return View();
+        }
+
+        //this is the second instance of the calculator so it hsa the GradeCalculatorModel and holds the inputs from the user
+        [HttpPost]
+        public IActionResult calculator(GradeCalculatorModel model)
+        {
+
+            return View();
+        }
+
+    }
+}
---
+++ Mission04_norty144/models/GradeCalculatorModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission04_norty144.models
+{
+    public class GradeCalculatorModel
+    {
+        //above each reference to an input there are two forms of validation for the user input. A range validation and making each input required.
+        [Required]
+        [Range(0, 100, ErrorMessage = "Entry must be between 0 and 100")]
+        public float assignments { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Entry must be between 0 and 100")]
+        public float group { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Entry must be between 0 and 100")]
+        public float quiz { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Entry must be between 0 and 100")]
+        public float midterm { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Entry must be between 0 and 100")]
+        public float final { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Entry must be between 0 and 100")]
+        public float intex { get; set; }
+        
+
+
+    }
+}
